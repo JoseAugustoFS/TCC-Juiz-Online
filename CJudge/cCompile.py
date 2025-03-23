7c24f94bb2caddefe5cc4f16a1690e8dced5dc99
MarcusVynicius1 <marcus.p@gsuite.iff.edu.br>
2025-03-23 12:23:08 -0300

    Padronizando a classe.

--- CJudge/cCompile.py
+++ CJudge/cCompile.py
@@ -15,10 +15,9 @@ def read_and_display_c_file(file_path):
 
 def compile_c_file(file_path):
     try:
-        # Definir o caminho para o executável
         output_file = os.path.splitext(file_path)[
             0
-        ]  # Remove a extensão ".c" para gerar o executável
+        ]
         result = subprocess.run(
             ["gcc", file_path, "-o", output_file], capture_output=True, text=True
         )
@@ -27,7 +26,7 @@ def compile_c_file(file_path):
             print(
                 f"Compilação bem-sucedida. Arquivo executável gerado: {output_file}.exe"
             )
-            return f"{output_file}.exe"  # Retorna o caminho do executável gerado
+            return f"{output_file}.exe"
         else:
             print(f"Erro na compilação:\n{result.stderr}")
             return None
@@ -38,7 +37,7 @@ def compile_c_file(file_path):
 
 def execute_c_program(executable_path):
     try:
-        # Executar o programa compilado
+
         result = subprocess.run([executable_path], capture_output=True, text=True)
         print("Saída do programa:")
         print(result.stdout)
@@ -50,14 +49,11 @@ def execute_c_program(executable_path):
 
 
 if __name__ == "__main__":
-    # Caminho para o arquivo C
+
     c_file_path = r"C:\exemple1.c"
 
-    # Passo 1: Ler e exibir o conteúdo do arquivo C
     if read_and_display_c_file(c_file_path):
-        # Passo 2: Compilar o arquivo C
         executable_path = compile_c_file(c_file_path)
 
         if executable_path:
-            # Passo 3: Executar o programa compilado
             execute_c_program(executable_path)
